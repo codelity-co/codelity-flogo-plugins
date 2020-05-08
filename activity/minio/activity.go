f459bffd961cd9655a0b6948fca74b735071681e
reidlai <reidlai@gmail.com>
2020-05-08 10:33:45 +0800

    fix PutObject uint8 error

--- activity/minio/activity.go
+++ activity/minio/activity.go
@@ -2,16 +2,16 @@ package minio
 
 import (
 	"bytes"
+	"encoding/json"
 
 	"github.com/project-flogo/core/activity"
-	"github.com/project-flogo/core/data/metadata"
 	"github.com/project-flogo/core/support/log"
 
 	"github.com/minio/minio-go/v6"
 )
 
 func init() {
-	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+	_ = activity.Register(&Activity{}, New) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
 }
 
 var activityMd = activity.ToMetadata(&Settings{}, &Input{}, &Output{})
@@ -26,7 +26,7 @@ func New(ctx activity.InitContext) (activity.Activity, error) {
 	)
 
 	s := &Settings{}
-	err := metadata.MapToStruct(ctx.Settings(), s, true)
+	err := s.FromMap(ctx.Settings())
 	if err != nil {
 		return nil, err
 	}
@@ -68,35 +68,55 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 		return true, err
 	}
 
-	a.logger.Debugf("ObjectName: %v", input.ObjectName)
-	a.logger.Debugf("Data: %v", input.Data)
+	a.logger.Debugf("Input: %v", input)
 
-	result := make(map[string]interface{})
 	output := &Output{}
+	dataBytes := getDataBytes(input.Data)
 	switch a.activitySettings.MethodName {
 	case "PutObject":
-		dataBytes := getDataBytes(a.activitySettings.DataType, input.Data)
 		numberOfBytes, err := a.minioClient.PutObject(a.activitySettings.BucketName, input.ObjectName, bytes.NewReader(dataBytes), int64(len(dataBytes)), minio.PutObjectOptions{})
 		if err != nil {
+			_ = output.FromMap(map[string]interface{}{
+				"status": "ERROR",
+				"result": map[string]interface{}{
+					"error": err.Error(),
+				},
+			})
+			_ = ctx.SetOutputObject(output)
 			return true, err
 		}
-		result["bytes"] = numberOfBytes
-		output.Status = "SUCCESS"
-		output.Result = result
-	}
-	
-	err = ctx.SetOutputObject(output)
-	if err != nil {
-		return true, err
+		if err = output.FromMap(map[string]interface{}{
+			"status": "SUCCESS",
+			"result": map[string]interface{}{
+				"bytes": numberOfBytes,
+			},
+		}); err != nil {
+			_ = output.FromMap(map[string]interface{}{
+				"status": "ERROR",
+				"result": map[string]interface{}{
+					"error": err.Error(),
+				},
+			})
+			_ = ctx.SetOutputObject(output)
+			return true, err
+		}
+		if err := ctx.SetOutputObject(output); err != nil {
+			return true, err
+		}
 	}
 
 	return true, nil
 }
 
-func getDataBytes(dataType string, data interface{}) []uint8 {
-	switch dataType {
-	case "string":
-		return []uint8(data.(string))
+func getDataBytes(data interface{}) []byte {
+	switch data.(type) {
+	case string:
+		return []byte(data.(string))
+	case map[string]interface{}:
+		dataBytes, _ := json.Marshal(data.(map[string]interface{}))
+		return dataBytes
+	case []byte:
+		return data.([]byte)
 	}
 	return nil
 }
--- activity/minio/metadata.go
+++ activity/minio/metadata.go
@@ -1,5 +1,10 @@
 package minio
 
+import (
+	"github.com/project-flogo/core/data/coerce"
+	"github.com/project-flogo/core/app/resolve"
+)
+
 type Settings struct {
 	Endpoint string `md:"endpoint,required"`
 	AccessKey string `md:"accessKey,required"`
@@ -8,8 +13,114 @@ type Settings struct {
 	BucketName string `md:"bucketName,required"`
 	Region string `md:"region"`
 	MethodName string `md:"methodName,required"` 
-	MethodOptions map[string]interface{} `md:"methodOpts"`
+	MethodOptions map[string]interface{} `md:"methodOptions"`
 	DataType string `md:"dataType,required"`
+}
+
+func (s *Settings) FromMap(values map[string]interface{}) error {
+
+	var (
+		err error
+	)
+
+	s.Endpoint, err = coerce.ToString(values["endpoint"])
+	if err != nil {
+		return err
+	}
+
+	s.AccessKey, err = coerce.ToString(values["accessKey"])
+	if err != nil {
+		return err
+	}
+
+	s.SecretKey, err = coerce.ToString(values["secretKey"])
+	if err != nil {
+		return err
+	}
+
+	s.EnableSsl, err = coerce.ToBool(values["enableSsl"])
+	if err != nil {
+		return err
+	}
+
+	s.BucketName, err = coerce.ToString(values["bucketName"])
+	if err != nil {
+		return err
+	}
+
+	s.Region, err = coerce.ToString(values["region"])
+	if err != nil {
+		return err
+	}
+
+	s.MethodName, err = coerce.ToString(values["methodName"])
+	if err != nil {
+		return err
+	}
+
+	s.DataType, err = coerce.ToString(values["dataType"])
+	if err != nil {
+		return err
+	}
+
+	if values["methodOptions"] != nil {
+		s.MethodOptions = make(map[string]interface{})
+		for k, v := range values["methodOptions"].(map[string]interface{}) {
+			s.MethodOptions[k], err = s.MapValue(v)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+
+}
+
+func (s *Settings) ToMap() map[string]interface{} {
+
+	return map[string]interface{}{
+		"endpoint": s.Endpoint,
+		"accessKey":    s.AccessKey,
+		"secretKey": s.SecretKey,
+		"enableSsl": s.EnableSsl,
+		"bucketName": s.BucketName,
+		"region": s.Region,
+		"methodName": s.MethodName,
+		"methodOptions": s.MethodOptions,
+		"dataType":    s.DataType,
+	}
+
+}
+
+func (s *Settings) MapValue(value interface{}) (interface{}, error) {
+	var (
+		err      error
+		anyValue interface{}
+	)
+
+	switch val := value.(type) {
+	case string:
+		strVal := val
+		if len(strVal) > 0 && strVal[0] == '=' {
+			anyValue, err = resolve.Resolve(strVal[1:], nil)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			anyValue, err = coerce.ToAny(val)
+			if err != nil {
+				return nil, err
+			}
+		}
+	default:
+		anyValue, err = coerce.ToAny(val)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return anyValue, nil
 }
 
 type Input struct {
@@ -18,8 +129,15 @@ type Input struct {
 }
 
 func (r *Input) FromMap(values map[string]interface{}) error {
-	r.ObjectName, _ = values["objectName"].(string)
+	var err error
+
+	r.ObjectName, err = coerce.ToString(values["objectName"])
+	if err != nil {
+		return err
+	}
+
 	r.Data = values["data"]
+	
 	return nil
 }
 
